42fcc90efc5b34a2fe81ce0da3cd817b58d46509
Emi <dr.emi.tanaka@gmail.com>
2022-12-04 14:23:13 +1100

    skeleton website

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+This repo contains the materials for the Data Visualisation with R workshop held online for WOMBAT on Tuesday 6th November 2022. 
+
+## Website
+
+The website can be viewed at [emitanaka.org/EDVR-workshop](https://emitanaka.org/EDVR-workshop) or by downloading this repo and opening `docs/index.html`. 
+
+## Quick survey
+
+If you use my teaching materials, I'll appreciate if you could fill out [this very short survey](https://docs.google.com/forms/d/e/1FAIpQLSdVnpDphyWnBeMw21Rs8vMySi5RGYN7kQh_VHU6rT83t8CDbA/viewform) to let me know! 
---
+++ pkg/R/data.R
@@ -0,0 +1,44 @@
+#' Hospital treatment coverage data
+#'
+#' The data shows the quarterly hospital treatment coverage by state or territory
+#' as insured persons as percentage of population from 1970s to September 2022.
+#'
+#' @source Australian Prudential Regulation Authority (APRA),
+#' accessed on 03/12/2022.
+"apra_hospital_coverage"
+
+#' Birth place by sex from the Australian Census
+#'
+#' ABS data used with permission from the Australian Bureau of Statistics.
+#' The data is taken from the datapack from the ABS website for the 2011,
+#' 2016, and 2021 Australian Census.
+#'
+#' @source Australian Census 2011, 2016 and 2021, Table 09 from the
+#'   Australian Bureau of Statistics \url{www.abs.gov.au}. Accessed on
+#'   03/12/2022.
+"census_bpsex"
+
+#' Summary residential community emissions
+#'
+#' The dataset contains residential energy (electricity and gas) consumption
+#' by suburb and by customer as well as associated GHG emissions.
+#'
+#' @source City of Casey \url{https://data.casey.vic.gov.au/}
+"casey_emissions"
+
+#' Covid-19 data for Australia
+#'
+#' The data is the merged data from the Johns Hopkins University CSSE team.
+#'
+#' @source Data extracted from the `tidycovid19` package using
+#' `download_merged_data(cache = TRUE)`. Accessed on 03/12/22.
+"au_covid"
+
+#' EPA Air Watch Data
+#'
+#' The data shows air quality information measured hourly by EPA’s air monitoring
+#' stations around Victoria.
+#'
+#' @source Environmental Protection Agency, Accessed on 03/12/2022.
+#' \url{https://discover.data.vic.gov.au/dataset/epa-air-watch-all-sites-air-quality-hourly-averages-yearly}
+"epa_airquality"
